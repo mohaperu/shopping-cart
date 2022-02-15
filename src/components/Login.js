31942c89262466bbcd09cc0b0efda881397bbf5c
mohaperu <mohaperu@gmail.com>
2022-02-15 23:22:40 +0530

    login Changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom';
 const Login = () => {
 
   const [data, setData] = useState({
-    email: "",
-    password: ""
+    email: "test@gmail.com",
+    password: "Admin123@"
   });
 
   const handleChange = ({ currentTarget: input }) => {
